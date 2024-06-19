79ab2bc84fc21db8cb40b9a1a6251b1691339771
iankelwin <krochaalmeida7@gmail.com>
2024-06-19 14:41:14 -0300

    create interface.kv
    to write interface code

---
+++ main.py
@@ -0,0 +1,6 @@
+#importing dependences
+from kivymd.app import MDApp as App
+
+from kivy.lang import Builder as Bd
+
+gui = Bd.load_file('interface.kv') #carrega arquvo .kv e transforma na interface
